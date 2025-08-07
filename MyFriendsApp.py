7a383b49e550a0300e1f2f80bc954c14a4e0cd66
Ana Gonzalez <157073381+anagonzalez94@users.noreply.github.com>
2025-08-07 17:03:53 +0000

    initial commit

--- MyFriendsApp.py
+++ MyFriendsApp.py
@@ -1,1 +1,23 @@
 from Friend import Friend
+from Birthday import Birthday
+
+friendlist = []
+
+def createFriend():
+    firstName = input("First name: ")
+    lastName = input("Last name: ")
+    friend = Friend(firstName, lastName)
+    birthDay = input("Birthday day: ")
+    birthMonth = input("Birthday month (1-12): ")
+
+# Main menu loop
+def mainMenu():
+    print("1 - Create new friend record")
+    print("2 - Search for a friend")
+    print("3 - Run reports")
+    print("4 - Exit")
+    selection = input("Enter your selection: ")
+
+# Startup
+if __name__ == "__main__":
+    mainMenu()
